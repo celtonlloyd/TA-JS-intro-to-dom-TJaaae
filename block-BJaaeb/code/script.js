3ec71384f737da4bcda9c8d5eb47a9a8ad52d9db
Elton <celtonlloyd@gmail.com>
2020-09-11 02:46:47 +0530

    block 4 last question left

--- block-BJaaeb/code/script.js
+++ block-BJaaeb/code/script.js
@@ -6,7 +6,15 @@ default value to be "text" and return the input element inside label. (create it
 */
 
 // Your code goes here
-
+let labelElement = document.createElement("label");
+document.body.append(labelElement);
+let inputElement = document.createElement("input");
+function createInputElm(label, type = "text") {
+  labelElement.innerText = label + ":";
+  inputElement.type = type;
+  labelElement.append(inputElement);
+  return labelElement;
+}
 // TEST
 createInputElm('Your name'); //<label>Your name: <input type="text"></label>
 createInputElm('Your age', 'number'); //<label>Your age: <input type="number"></label>
@@ -14,20 +22,32 @@ createInputElm('Your age', 'number'); //<label>Your age: <input type="number"></
 // 2. Do the same thing as above using string literal like `<h1>Hello</h1>`
 
 // Your code goes here
-
+function createInputElm(label, type = "text") {
+  labelElement.innerHTML = `${label}: <input type=${type}>`;
+  return labelElement;
+}
 // TEST
 createInputElm('Your name'); //<label>Your name: <input type="text"></label>
 createInputElm('Your age', 'number'); //<label>Your age: <input type="number"></label>
 
-// 3. Create a function named `createList` that accept and array of data like ['Mango', 'Apple', 'Banana'] and returns
+// 3. Create a function named `createList` that accept an array of data like ['Mango', 'Apple', 'Banana'] and returns
 // the html for the link like <ul> <li>Mango</li>  <li>Apple</li>  <li>Banana</li> </ul>
 // Your code goes here
+function createList (arr)  {
+  let myUl = document.createElement("ul");
+  document.body.append(myUl);
 
+  arr.forEach((val) => {
+    let newLi = document.createElement("li");
+    newLi.innerText = val;
+    myUl.append(newLi);
+  });
+};
 // TEST
 createList(['ALABAMA', 'ALASKA', 'HAWAII', 'KENTUCKY']);
 createList(['Afghanistan', 'Antarctica', 'Congo', 'Estonia']);
 
-// 4. Create a function named `createTodoList` that accept and array of data like [{name: "Learn DOM", isDone: false}, {name: "Learn JS", isDone: true}] and returns
+// 4. Create a function named `createTodoList` that accept an array of data like [{name: "Learn DOM", isDone: false}, {name: "Learn JS", isDone: true}] and returns
 // the html for single todo will look like given below
 /* 
 <ul>

0a62a310a4b13c426e6d29d6ddd9d57a47ca27c9
Elton <celtonlloyd@gmail.com>
2020-09-14 19:09:02 +0530

    completed block 4 dynamic clock

---
+++ block-BJaaeh/code/script.js
@@ -0,0 +1,25 @@
+function currentTime() {
+    let date = new Date(); /* creating object of Date class */
+    let hour = date.getHours();
+    let min = date.getMinutes();
+    let sec = date.getSeconds();
+    hour = updateTime(hour);
+    min = updateTime(min);
+    sec = updateTime(sec);
+    document.querySelector(".clock").innerText =
+        hour + " : " + min + " : " + sec; /* adding time to the div */
+    setTimeout(function () {
+        currentTime();
+    }
+    , 1000); /* setting timer */
+}
+
+function updateTime(k) {
+  if (k < 10) {
+    return "0" + k;
+  } else {
+    return k;
+  }
+}
+
+currentTime(); /* calling currentTime() function to initiate the process */
